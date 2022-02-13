66cafe02540ef39a382600b32305bb60790b64a3
carlosalg <cabetolg98@gmail.com>
2022-02-13 16:23:40 -0500

    array updated

--- arrayss.go
+++ arrayss.go
@@ -15,11 +15,11 @@ func main() {
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
-	var twoO [2][3]int
+	var twoD [2][3]int
 	for i := 0; i < 2; i++ {
-		for j := 0; i < 2; i++ {
-			twoO[i][j] = i + j
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
 		}
 	}
-	fmt.Println("2d: ", twoO)
+	fmt.Println("2d: ", twoD)
 }
